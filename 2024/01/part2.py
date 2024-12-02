dcc7db7701832c3639a3b3a49a071a1abaa63e79
ordinaryindustries <132616209+ordinaryindustries@users.noreply.github.com>
2024-12-02 05:08:19 -0500

    Over optimize

--- 2024/01/part2.py
+++ 2024/01/part2.py
@@ -1,24 +1,6 @@
 from collections import Counter
 
 
-def load_input():
-    with open("input.txt") as f:
-        for line in f:
-            yield map(int, line.split())
-
-
-def main():
-    left_column = []
-    right_column = []
-
-    for left, right in load_input():
-        left_column.append(left)
-        right_column.append(right)
-
-    right_counts = Counter(right_column)
-
-    return sum(location * right_counts.get(location, 0) for location in left_column)
-
-
-if __name__ == "__main__":
-    print(main())
+with open("input.txt") as f:
+    left, right = zip(*(map(int, line.split()) for line in f))
+    print(sum(x * Counter(right).get(x, 0) for x in left))
